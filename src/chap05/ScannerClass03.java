0a295e69ca1ab2ccb0f879f5e8b61b167ab766ed
Kukyeon <rnrdus2000@naver.com>
2025-07-21 12:56:44 +0900

    숫자 게임

--- src/chap05/ScannerClass03.java
+++ src/chap05/ScannerClass03.java
@@ -16,8 +16,14 @@ public class ScannerClass03 {
 		System.out.println("숫자 맞추기");
 		System.out.println(" 1 부터 100 사이에 숫자를 맞춰 보기 ");
 		
+		int count = 0; // 횟수 추가
+		
 		while(true) {
+			
+			count++;
+			
 			System.out.println(" 1 부터 100 사이에 숫자를 맞춰 보기 ( 0 입력시 종료 ) : ");
+			
 			
 			int usernum = scanner.nextInt(); // 사용자 입력 숫자
 			if(usernum == 0) break;
@@ -26,7 +32,7 @@ public class ScannerClass03 {
 			}else if(usernum < comNumber) {
 				System.out.println("당신의 숫자가 더 작습니다. ");
 			} else {
-				System.out.println("숫자가 같습니다.");
+				System.out.println("숫자가 같습니다."+count+"회 사용");
 				break;
 			} 
 			
