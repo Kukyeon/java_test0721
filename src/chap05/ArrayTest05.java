1215f167d3a2ed121202c5b64f9cca4d241504ca
Kukyeon <rnrdus2000@naver.com>
2025-07-21 16:49:37 +0900

    array 추가

---
+++ src/chap05/ArrayTest05.java
@@ -0,0 +1,40 @@
+package chap05;
+
+public class ArrayTest05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		// 향상된 for 문
+		
+		int[] arr = {10, 20, 30, 40, 50};
+		
+		//모든 배열 요소를 출력하는 for문 작성 전환전
+		
+		for(int i=0 ; i<arr.length ; i++) {
+			System.out.println(arr[i]);
+		}
+		System.out.println("========================");
+		// 향상된 for문으로 전환
+		
+		for( int num : arr) { // 배열 요소를 차례대로 나열
+			System.out.println(num);
+		}
+		System.out.println("========================");
+		// 다음 배열의 모든 요소 중 짝수만 출력하시오.
+		
+		int[] numArr = {10,20,30,11,22,33,44,55};
+		
+		for(int num : numArr) {
+			if(num % 2 == 0) {
+				System.out.println(num);
+			}
+		}
+		
+		
+		
+		
+	}
+
+}
